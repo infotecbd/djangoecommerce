ef07298c170bde42fa03b910f1dbce31e49129e8
infotecbd <66203215+infotecbd@users.noreply.github.com>
2025-05-23 12:18:25 +0600

    Upadated

--- FastKart/settings.py
+++ FastKart/settings.py
@@ -152,8 +152,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "mdhossain.odesk@gmail.com" #env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = "moql mdfw grvt ztvp" #env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = "mdhossain@gmail.com" #env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = "moql mdfw" #env("EMAIL_HOST_PASSWORD")
 
 # Payment Configuration
 
